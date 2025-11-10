25b650c944983c20e2592aad3fec027769c850a4
lajhinfo-creator <lajh.info@gmail.com>
2025-11-10 11:58:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function handleFooterZoomParallax() {
 
   // Zoom: from 120% -> 100%
   const startZoom = 120;
-  const endZoom = 60;
+  const endZoom = 75;
   const currentZoom = startZoom - (startZoom - endZoom) * progress;
 
   // Vertical position: from 90% -> 70% (moves focus upward)
